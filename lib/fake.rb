e28bbd5c1212462addf6d4b48dfe4552266bf81d
Piotr Zolnierek <pz@anixe.pl>
2011-02-15 06:33:20 +0100

    [FIXED] put into Fake namespace, bumped version

--- lib/fake.rb
+++ lib/fake.rb
@@ -1,35 +1,36 @@
-class Fake < BasicObject
+module Fake
+  class Fake < BasicObject
 
-	def initialize
-		@hash = {}
-	end
+  	def initialize
+  		@hash = {}
+  	end
 
-	def self.create(args={})
-		fake = ::Fake.new
-		args.each {|k, v| fake.stub!(k, v) }
-		fake
-	end
+  	def self.create(args={})
+  		fake = ::Fake.new
+  		args.each {|k, v| fake.stub!(k, v) }
+  		fake
+  	end
 
-	def stub!(m, v, *args)
-		@hash[__key__(m, args)] = v
-	end
+  	def stub!(m, v, *args)
+  		@hash[__key__(m, args)] = v
+  	end
 
-	def method_missing(m, *args)
-		@hash[__key__(m, args)] or raise_no_metho_error(m)
-	end
+  	def method_missing(m, *args)
+  		@hash[__key__(m, args)] or raise_no_metho_error(m)
+  	end
 
-	def inspect
-		@hash
-	end
+  	def inspect
+  		@hash
+  	end
 	
-	private
-		def __key__(m, *args)
-			[m, *args]
-		end
+  	private
+  		def __key__(m, *args)
+  			[m, *args]
+  		end
 
-		def raise_no_metho_error(m)
-			::Kernel.raise(::NoMethodError, "undefined method '#{m}'")
-		end
+  		def raise_no_metho_error(m)
+  			::Kernel.raise(::NoMethodError, "undefined method '#{m}'")
+  		end
 
+  end
 end
-
--- lib/fake/version.rb
+++ lib/fake/version.rb
@@ -1,3 +1,3 @@
-class Fake
-  VERSION = "0.0.2"
+module Fake
+  VERSION = "0.0.3"
 end
